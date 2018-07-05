eaef6285b2df0a74960cbabd5a183516f73cf43e
Bruno Casali <brunoocasali@gmail.com>
2018-07-05 20:52:34 -0300

    Use common names in node world

--- src/index.js
+++ src/index.js
@@ -15,15 +15,14 @@ function gulpMinio(bucket, config) {
   let client = new Client(config)
 
   return es.map((file, cb) => {
-    if (file.isDirectory()){
+    if (file.isDirectory())
       return;
-    }
 
     let meta = { 'Content-Type': lookup(file.path) };
 
     client.fPutObject(bucket, file.relative, file.path, meta, err => {
       if (err)
-        throw new PluginError(PLUGIN_NAME, 'Error processing file to minio');
+        throw new PluginError(PLUGIN_NAME, `Error processing ${file} to minio`);
     });
 
     return cb();
